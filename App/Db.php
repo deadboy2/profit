1a03b3f95529904df8987366c007c6008614479e
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-08 18:59:25 +0300

    create and initialize method query(..) for execute data from database

--- App/Db.php
+++ App/Db.php
@@ -17,4 +17,14 @@ class Db
         $res = $statement->execute();
         return $res;
     }
+
+    public function query($query)
+    {
+        $statement = $this->conn->prepare($query);
+        $res = $statement->execute();
+        if (false !== $res) {
+            return $statement->fetchAll();
+        }
+        return [];
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
 require __DIR__ . '/autoload.php';
+
+$db = new \App\Db();
+$res = $db->query('select * from foo');

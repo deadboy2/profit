fb888cf3cfea5d1f459eca504a2f63f52f3e2492
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-08 18:22:24 +0300

    modify index.php!!!

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <title>Index</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-</head>
-<body>
+<?php
 
-
-<script src="js/bootstrap.min.js"></script>
-<script type="text/javascript">
-    $(function () {
-
-    });
-</script>
-</body>
-</html>
+require __DIR__ . '/autoload.php';

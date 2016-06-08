44a097b145512b38b9a08aa7b392b3aa8d5b37bf
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-08 18:44:44 +0300

    add connection and method ex(..).Create query in db.

--- App/Db.php
+++ App/Db.php
@@ -4,5 +4,17 @@ namespace App;
 
 class Db
 {
+    protected $conn;
 
+    function __construct()
+    {
+        $this->conn = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+    }
+
+    public function ex($query)
+    {
+        $statement = $this->conn->prepare($query);
+        $res = $statement->execute();
+        return $res;
+    }
 }

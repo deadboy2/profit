ca84021598e9c6064950da22fd1dea4037f8a2dc
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-25 11:13:51 +0300

    + mod_rewrite, .htaccess

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 require __DIR__ . '/autoload.php';
+
+$url = $_SERVER['REQUEST_URI'];
 
 $controller = new \App\Controllers\News();
 
--- one.php
+++ one.php
@@ -1,6 +1,4 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
 require __DIR__ . '/autoload.php';
-
 $controller = new \App\Controllers\News();
 $controller->action('One');

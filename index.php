38f4d4119ac7b5c891a5797d4c3a915f96bc030f
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-25 12:46:18 +0300

    + front full

--- index.php
+++ index.php
@@ -2,9 +2,24 @@
 require __DIR__ . '/autoload.php';
 
 $url = $_SERVER['REQUEST_URI'];
+$act = '';
+$id = '';
+
+if (isset($_GET['id'])) {
+    $id = (int)$_GET['id'];
+}
 
 $controller = new \App\Controllers\News();
 
-$action = $_GET['action'] ?: 'Index';
+switch ($url) {
+    case '/':
+        $act = 'Index';
+        break;
+    case '/news/?id=' . $id:
+        $act = 'One';
+        break;
+    default:
+        $act = 'Index';
+}
 
-$controller->action($action);
+$controller->action($act);
--- one.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/autoload.php';
-$controller = new \App\Controllers\News();
-$controller->action('One');

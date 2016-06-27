a5f3c3d487bc125f858e6104bbba9a796289b23e
Mihail Prokushev <mihailhrustalev8@gmail.com>
2016-06-27 20:48:27 +0300

    + modify

--- index.php
+++ index.php
@@ -4,18 +4,13 @@ require __DIR__ . '/autoload.php';
 $url = $_SERVER['REQUEST_URI'];
 $newsController = new \App\Controllers\News();
 $errorController = new \App\Controllers\Error();
-$id = '';
 $art = '';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-}
 
 switch ($url) {
     case '/':
         $art = 'Index';
         break;
-    case '/news/?id=' . $id:
+    case '/news/?id=' . $_GET['id']:
         $art = 'One';
         break;
     default:
